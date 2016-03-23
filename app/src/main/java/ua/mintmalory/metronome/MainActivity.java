480d9ae5abfb335340c759188d5089ea50620bbd
MintMalory <lugyshal@voliacable.com>
2016-03-24 01:59:04 +0200

    Edited .gitignore file

--- app/src/main/java/ua/mintmalory/metronome/MainActivity.java
+++ app/src/main/java/ua/mintmalory/metronome/MainActivity.java
@@ -18,85 +18,83 @@ import android.widget.ToggleButton;
 
 
 public class MainActivity extends AppCompatActivity {
-	private static final String TAG = MainActivity.class.toString();
-	
-	private ToggleButton mVibrationToggleButton;
-	private ToggleButton mFlashToggleButton;
-	private ToggleButton mSoundToggleButton;
-		
-	private ToggleButton mStartStopToggleButton;
+    private static final String TAG = MainActivity.class.toString();
+
+    private ToggleButton mVibrationToggleButton;
+    private ToggleButton mFlashToggleButton;
+    private ToggleButton mSoundToggleButton;
+
+    private ToggleButton mStartStopToggleButton;
     private SeekBar mBmpSeekBar;
     private EditText mBmpEditText;
     private ImageView mIndicatorImageView;
-	private BroadcastReceiver mMetronomeServiceReceiver;
-	
-	
+    private BroadcastReceiver mMetronomeServiceReceiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-		initViews();
-		initBroadcastReceiver();
-		initManualModeButtonsListeners();
-		initBpmChangersListeners();
+        initViews();
+        initBroadcastReceiver();
+        initManualModeButtonsListeners();
+        initBpmChangersListeners();
         initStartBtnListener();
     }
 
-	private void initViews(){
-		mVibrationToggleButton = (ToggleButton) findViewById(R.id.vibro_btn);
-		mFlashToggleButton = (ToggleButton) findViewById(R.id.start_stop_button);
-		mSoundToggleButton = (ToggleButton) findViewById(R.id.sound_btn);
-		
+    private void initViews() {
+        mVibrationToggleButton = (ToggleButton) findViewById(R.id.vibro_btn);
+        mFlashToggleButton = (ToggleButton) findViewById(R.id.start_stop_button);
+        mSoundToggleButton = (ToggleButton) findViewById(R.id.sound_btn);
         mStartStopToggleButton = (ToggleButton) findViewById(R.id.start_stop_button);
         mBmpSeekBar = (SeekBar) findViewById(R.id.seekBar);
         mBmpEditText = (EditText) findViewById(R.id.bpm_editText);
         mIndicatorImageView = (ImageView) findViewById(R.id.indicator_imgView);
-	}
-	
-	private void initBroadcastReceiver(){
-		mMetronomeServiceReceiver = new BroadcastReceiver() {
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				boolean flashIsOn = intent.getBooleanExtra(MetronomeService.INDICATOR_STATE_EXTRA, false);
+    }
 
-				if (flashIsOn){
-					mIndicatorImageView.setBackgroundDrawable(getResources().getDrawable(R.drawable.indicator_on));
-				}else{
-					mIndicatorImageView.setBackgroundDrawable(getResources().getDrawable(R.drawable.indicator_off));
-				}
-			}
-		};
-		
-		registerReceiver(mMetronomeServiceReceiver, new IntentFilter(MetronomeService.BROADCAST_ACTION));
-	}
+    private void initBroadcastReceiver() {
+        mMetronomeServiceReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                boolean indicatorIsOn = intent.getBooleanExtra(MetronomeService.INDICATOR_STATE_EXTRA, false);
 
-	private void initManualModeButtonsListeners(){
-		mVibrationToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                if (indicatorIsOn) {
+                    mIndicatorImageView.setBackgroundDrawable(getResources().getDrawable(R.drawable.indicator_on));
+                } else {
+                    mIndicatorImageView.setBackgroundDrawable(getResources().getDrawable(R.drawable.indicator_off));
+                }
+            }
+        };
+
+        registerReceiver(mMetronomeServiceReceiver, new IntentFilter(MetronomeService.BROADCAST_ACTION));
+    }
+
+    private void initManualModeButtonsListeners() {
+        mVibrationToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 updateService();
             }
         });
-		
-		mFlashToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+        mFlashToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 updateService();
             }
         });
-		
+
         mSoundToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 updateService();
             }
         });
-	}
-	
-	private void initBpmChangersListeners(){
-		mBmpSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+    }
+
+    private void initBpmChangersListeners() {
+        mBmpSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
@@ -115,61 +113,67 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-       mBmpEditText.addTextChangedListener(new TextWatcher() {
-           @Override
-           public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-           }
+        mBmpEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-           @Override
-           public void onTextChanged(CharSequence s, int start, int before, int count) {
-           }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
 
-           @Override
-           public void afterTextChanged(Editable s) {
-               try {
-                   int i = Integer.parseInt(s.toString());
+            @Override
+            public void afterTextChanged(Editable s) {
+                try {
+                    int i = Integer.parseInt(s.toString());
 
-                   mBmpSeekBar.setProgress(i);
-                   updateService();
-               } catch (NumberFormatException e) {
-                   Log.d(TAG, e.getMessage());
-                   Toast toast = Toast.makeText(getApplicationContext(),
-                           "Only integer can be set as the bmp value!", Toast.LENGTH_LONG);
-                   toast.show();
+                    if (i > 208) {
+                        i = 208;
 
-                   mBmpEditText.setText(Integer.toString(mBmpSeekBar.getProgress()));
-               }
-           }
-       });
-	}
-	
-	private void initStartBtnListener(){
-		        mStartStopToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        updateService();
+                        mBmpEditText.setText(Integer.toString(i));
                     }
-                });
-	}
-	
-	private void updateService(){
-		Intent intent = new Intent(this, MetronomeService.class);
-		stopService(intent);
-		
-		if((mStartStopToggleButton.isChecked())&&(mBmpSeekBar.getProgress() > 0)){
-			intent.putExtra(MetronomeService.VIBRATION_EXTRA, mVibrationToggleButton.isChecked());
-			intent.putExtra(MetronomeService.FLASH_EXTRA, mFlashToggleButton.isChecked());
-			intent.putExtra(MetronomeService.SOUND_EXTRA, mSoundToggleButton.isChecked());
-			intent.putExtra(MetronomeService.BPM_EXTRA, mBmpSeekBar.getProgress());
-			
-			startService(intent);
-		}
-	}
-	
+
+                    mBmpSeekBar.setProgress(i);
+                    updateService();
+                } catch (NumberFormatException e) {
+                    Log.d(TAG, e.getMessage());
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Only integer can be set as the bmp value!", Toast.LENGTH_LONG);
+                    toast.show();
+
+                    mBmpEditText.setText(Integer.toString(mBmpSeekBar.getProgress()));
+                }
+            }
+        });
+    }
+
+    private void initStartBtnListener() {
+        mStartStopToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                updateService();
+            }
+        });
+    }
+
+    private void updateService() {
+        Intent intent = new Intent(this, MetronomeService.class);
+        stopService(intent);
+
+        if ((mStartStopToggleButton.isChecked()) && (mBmpSeekBar.getProgress() > 0)) {
+            intent.putExtra(MetronomeService.VIBRATION_EXTRA, mVibrationToggleButton.isChecked());
+            intent.putExtra(MetronomeService.FLASH_EXTRA, mFlashToggleButton.isChecked());
+            intent.putExtra(MetronomeService.SOUND_EXTRA, mSoundToggleButton.isChecked());
+            intent.putExtra(MetronomeService.BPM_EXTRA, mBmpSeekBar.getProgress());
+
+            startService(intent);
+        }
+    }
+
     @Override
     public void onBackPressed() {
         stopService(new Intent(this, MetronomeService.class));
         unregisterReceiver(mMetronomeServiceReceiver);
-		super.onBackPressed();
+        super.onBackPressed();
     }
 }
--- app/src/main/java/ua/mintmalory/metronome/MetronomeService.java
+++ app/src/main/java/ua/mintmalory/metronome/MetronomeService.java
@@ -20,10 +20,11 @@ public class MetronomeService extends Service {
     public static final String SOUND_EXTRA = MetronomeService.class + ".sound";
     public static final String BPM_EXTRA = MetronomeService.class + ".bpm";
     public static final String INDICATOR_STATE_EXTRA = MetronomeService.class + ".indicator";
+    public static final String BROADCAST_ACTION = MetronomeService.class + ".broadcast";
 
     private Timer timer;
-    private TimerTask tone; //rename
-    public static final String BROADCAST_ACTION = "ua.mintmalory.metronome";
+    private TimerTask oneIteration;
+    private Camera camera;
     private Intent intent;
     private Handler handler;
 
@@ -32,7 +33,7 @@ public class MetronomeService extends Service {
     private boolean flashOn = false;
     private boolean soundOn = false;
 
-    private Camera camera;
+
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -50,14 +51,12 @@ public class MetronomeService extends Service {
 
         if (CAMERA_IS_AVAILABLE && flashOn) {
             camera = Camera.open();
-            Camera.Parameters p = camera.getParameters();
-            p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            camera.setParameters(p);
         }
 
-        tone = new TimerTask() {
+        oneIteration = new TimerTask() {
             private ToneGenerator tick = new ToneGenerator(AudioManager.STREAM_MUSIC, 100); //tickSound
             private Vibrator v = (Vibrator) getSystemService(getApplicationContext().VIBRATOR_SERVICE); //vibrator
+            private Camera.Parameters p;
 
             @Override
             public void run() {
@@ -65,6 +64,9 @@ public class MetronomeService extends Service {
                 sendBroadcast(intent);
 
                 if (CAMERA_IS_AVAILABLE && flashOn) {
+                    p = camera.getParameters();
+                    p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    camera.setParameters(p);
                     camera.startPreview();
                 }
 
@@ -77,28 +79,31 @@ public class MetronomeService extends Service {
                 }
 
                 if (CAMERA_IS_AVAILABLE && flashOn) {
+                    p = camera.getParameters();
+                    p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                    camera.setParameters(p);
                     camera.stopPreview();
                 }
-
 
                 intent.putExtra(INDICATOR_STATE_EXTRA, false);
                 sendBroadcast(intent);
             }
         };
 
-        handler.removeCallbacks(tone);
-        handler.postAtFrontOfQueue(tone);
+        handler.removeCallbacks(oneIteration);
+        handler.postAtFrontOfQueue(oneIteration);
     }
 
     @Override
     public void onDestroy() {
         timer.cancel();
         timer.purge();
+
         if (camera!=null) {
             camera.release();
         }
-        handler.removeCallbacks(tone);
 
+        handler.removeCallbacks(oneIteration);
     }
 
 
@@ -110,7 +115,7 @@ public class MetronomeService extends Service {
 
         int bpm = intent.getIntExtra(BPM_EXTRA, 100);
 
-        timer.schedule(tone, new Date(), 60000 / bpm);
+        timer.schedule(oneIteration, new Date(), 60000 / bpm);
 
         return super.onStartCommand(intent, flags, startId);
     }
